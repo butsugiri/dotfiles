2b32fe598067643b5952338322a1371e6c86f505
Butsugiri <kiyono@hakukin.local>
2016-02-21 17:17:40 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf ~/Dropbox/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/Dropbox/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/Dropbox/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/Dropbox/dotfiles/.zshrc ~/.zshrc
